3477b626d280ef90201101b1e60791a29b766cdf
Andrew Bloomgarden <andrew@newrelic.com>
2013-09-16 16:35:49 -0700

    Remove substringing of SNS->SQS message
    It removes the opening & trailing curly braces.

--- src/main/java/com/base2services/jenkins/github/GitHubTriggerProcessor.java
+++ src/main/java/com/base2services/jenkins/github/GitHubTriggerProcessor.java
@@ -77,7 +77,6 @@ public class GitHubTriggerProcessor implements TriggerProcessor {
         if(json.has("Type")) {
             String msg = json.getString("Message");
             if(msg != null) {
-                msg = msg.substring(1,msg.length()-1); //remove the leading and trailing double quotes
                 return JSONObject.fromObject(msg);
             }
         } else if (json.has("repository")){
